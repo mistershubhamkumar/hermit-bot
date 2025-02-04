79b4e98c0ee6241826a2d02aa9929a5c6ae68822
Shubham Kumar <kumaruikeshubham@gmail.com>
2025-02-04 19:53:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ const DEBUG = convertToBool(process.env.DEBUG, 'true');
 
 module.exports = {
   VERSION: 'v4.4.4',
-  SESSION_ID: process.env.SESSION_ID || '',
+  SESSION_ID: process.env.SESSION_ID || 'HP_2025_FEB_4_TA0YXI',
   MODE: (process.env.MODE || 'private').toLowerCase(),
   HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
   SEND_READ: toBool(process.env.READ_COMMAND),
